d7af4cb8b71719645dd3057c7052cfa03e86c2bf
albertomanzano <alberto.manzano.herrero@gmail.com>
2020-02-29 15:36:08 +0100

    Primera version de los constructores de triangulo

--- README.md
+++
@@ -1,2 +0,0 @@
-# Boletin3
-git clone
--- clase_punto.cpp
+++ clase_punto.cpp
@@ -4,27 +4,34 @@ using namespace std;
 
 // Constructores
 Punto::Punto(double x,double y){
+	cout<<"Creado un punto con coordenadas direccion "<<this<<" ";
 	this->x = x;
 	this->y = y;
+	this->print();
 }
 Punto::Punto(){
+	cout<<"Creado un punto por defecto direccion "<<this<<" ";
 	this->x = 0;
 	this->y = 0;
+	this->print();
 }
 
 // Rule of three
 // Constructor copia
 Punto::Punto(const Punto &p){
+	cout<<"Creado un punto por copia direccion "<<this<<" ";
 	this->x = p.x;
 	this->y = p.y;
+	this->print();
 }
 // Funcion destruir
 Punto::~Punto(){
-	cout<<"Destruido un punto"<<endl;
+	cout<<"Destruido un punto direccion "<<this<<endl;
 }
 // Funcion asignacion
 Punto& Punto::operator=(const Punto &p)
 {
+	cout<<"Asignado un punto direccion "<<this<<" a direccion: "<<&p<<endl;
 	if (this != &p ){
 		this->x = p.x;
 		this->y = p.y;
