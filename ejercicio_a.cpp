9ac2461f518a6cfb250f761e10d9eb22a6c45280
albertomanzano <alberto.manzano.herrero@gmail.com>
2020-03-03 11:35:10 +0100

    Area del cuadrado

--- ejercicio_a.cpp
+++ ejercicio_a.cpp
@@ -2,23 +2,34 @@
 #include "math.h"
 using namespace std;
 
+// Area del circulo
 double area(double radio){
 	return (M_PI*radio*radio);
 }
+
+// Area del cuadrado
+double area(float lado){
+	return (lado*lado);
+}
+
+// Area del rectangulo
 double area(double lado1,double lado2){
 	return (lado1*lado2);
 }
+
+// Area del trapecio
 double area(double h,
 	    double base1,
 	    double base2
 	    ){
 	return (0.5*h*(base1+base2));
-
 }
 
+
 int main(){
+char name[9] = "cuadrado";
 double a;
-a = area(1,4,4);
+a = area(1.f);
 cout<<"El area es: "<<a<<endl;
 
 }
