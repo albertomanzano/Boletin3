7861bd76acd9a436fd68dea12a9d1d2b9c9fb446
albertomanzano <alberto.manzano.herrero@gmail.com>
2020-03-03 12:56:04 +0100

    Funcion lectura overloaded para double float

--- ejercicio_b.cpp
+++ ejercicio_b.cpp
@@ -24,7 +24,7 @@ double sum(int n,double* &v){
 }
 
 // Lectura datos
-void* lectura(char filename[], int &n){
+void lectura(char filename[], int &n, int* &p){
 	ifstream fe;// creamos un fichero de entrada
 	fe.open(filename);//Apertura del fichero
 	if (fe.bad()){//Comprobamos errores
@@ -32,19 +32,48 @@ void* lectura(char filename[], int &n){
 	}
 	// Iteramos el fichero
 	fe>>n; // Dimension del fichero
-	int *p = new int[n];
+	p = new int[n];
 	for (int i=0;i<n;i++){
 	        fe>>p[i];
 	}
 	fe.close();
-	return p;
+}
+// Lectura datos
+void lectura(char filename[], int &n, double* &p){
+	ifstream fe;// creamos un fichero de entrada
+	fe.open(filename);//Apertura del fichero
+	if (fe.bad()){//Comprobamos errores
+	        cout << "ERROR: cannot open "<< filename<<endl;
+	}
+	// Iteramos el fichero
+	fe>>n; // Dimension del fichero
+	p = new double[n];
+	for (int i=0;i<n;i++){
+	        fe>>p[i];
+	}
+	fe.close();
+}
+// Lectura datos
+void lectura(char filename[], int &n, float* &p){
+	ifstream fe;// creamos un fichero de entrada
+	fe.open(filename);//Apertura del fichero
+	if (fe.bad()){//Comprobamos errores
+	        cout << "ERROR: cannot open "<< filename<<endl;
+	}
+	// Iteramos el fichero
+	fe>>n; // Dimension del fichero
+	p = new float[n];
+	for (int i=0;i<n;i++){
+	        fe>>p[i];
+	}
+	fe.close();
 }
 
 int main(){
 char filename[] = "datos.txt";
 int dim;
-int* p;
-p = lectura(filename,dim);
+float* p;
+lectura(filename,dim,p);
 for (int i = 0; i<dim; i++) cout<<p[i]<<endl;
 
 }
