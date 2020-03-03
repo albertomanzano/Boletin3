d3a39221064cc30164867a0825ef6e9744892ba1
albertomanzano <alberto.manzano.herrero@gmail.com>
2020-03-03 12:23:21 +0100

    Lectura de datos tipo int

--- ejercicio_b.cpp
+++ ejercicio_b.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 // Tipo int
@@ -22,13 +23,28 @@ double sum(int n,double* &v){
 	return suma;
 }
 
-int main(){
-int dim = 2000;
+// Lectura datos
+void* lectura(char filename[], int &n){
+	ifstream fe;// creamos un fichero de entrada
+	fe.open(filename);//Apertura del fichero
+	if (fe.bad()){//Comprobamos errores
+	        cout << "ERROR: cannot open "<< filename<<endl;
+	}
+	// Iteramos el fichero
+	fe>>n; // Dimension del fichero
+	int *p = new int[n];
+	for (int i=0;i<n;i++){
+	        fe>>p[i];
+	}
+	fe.close();
+	return p;
+}
 
-double suma1;
-double *p1 = new double[dim];
-for (int i=0;i<dim;i++) p1[i] = 0.5;
-suma1 = sum(dim,p1);
-cout<<"Suma double: "<<suma1<<endl;
+int main(){
+char filename[] = "datos.txt";
+int dim;
+int* p;
+p = lectura(filename,dim);
+for (int i = 0; i<dim; i++) cout<<p[i]<<endl;
 
 }
