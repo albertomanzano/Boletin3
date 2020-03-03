34e68996810ea3301011d52a2b7294d1de95356e
albertomanzano <alberto.manzano.herrero@gmail.com>
2020-03-03 11:06:36 +0100

    Area del trapecio

--- ejercicio_a.cpp
+++ ejercicio_a.cpp
@@ -8,10 +8,17 @@ double area(double radio){
 double area(double lado1,double lado2){
 	return (lado1*lado2);
 }
+double area(double h,
+	    double base1,
+	    double base2
+	    ){
+	return (0.5*h*(base1+base2));
+
+}
 
 int main(){
 double a;
-a = area(3,4);
+a = area(1,4,4);
 cout<<"El area es: "<<a<<endl;
 
 }
