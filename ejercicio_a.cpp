86dc99b7fc91790233e7b944fbb4f088433884f4
ciom2i <61544703+ciom2i@users.noreply.github.com>
2020-03-05 13:04:54 +0100

    Update ejercicio_a.cpp
    Añadiendo un argumento entero (numero de lados) para algunas geometrías, evitamos tener que usar floats en lugar doubles.

--- ejercicio_a.cpp
+++ ejercicio_a.cpp
@@ -3,37 +3,33 @@
 using namespace std;
 
 // Area del circulo
-double area(double radio){
+double area(double &radio){
 	return (M_PI*radio*radio);
 }
 
 // Area del cuadrado
-double area(float lado){
+double area(double &lado, int numl){
 	return (lado*lado);
 }
 
 // Area del rectangulo
-double area(double lado1,double lado2){
+double area(double &lado1, double &lado2){
 	return (lado1*lado2);
 }
 
 //Area del triangulo
-double area(float base, float altura){
-	return (base*altura/2.);
+double area(double &base, double &altura, int numl){
+	return (base*altura / 2.0);
 }
 
 // Area del trapecio
-double area(double h,
-	    double base1,
-	    double base2
-	    ){
-	return (0.5*h*(base1+base2));
+double area(double &h, double &base1, double &base2){
+	return (0.5*h*(base1 + base2));
 }
 
 
 int main(){
-double a;
-a = area(1.f,2.f);
-cout<<"El area es: "<<a<<endl;
+	double b = 1.0, h = 2.0;
+	cout<< "El area del rectangulo es: "<< area(b,h)<< endl;
 
 }
