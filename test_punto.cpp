88f0a17237623ae3fc3f224af134005d37960c64
albertomanzano <alberto.manzano.herrero@gmail.com>
2020-03-05 16:41:18 +0100

    Revisados test punto y triangulo

--- test_punto.cpp
+++ test_punto.cpp
@@ -4,14 +4,23 @@
 using namespace std;
 
 int main(){
-Punto p1 = Punto(2,3);
-Punto p2 = Punto(p1);
 
-cout<<"Al principio p2 vale"<<endl;
+cout<<"Punto0 por defecto"<<endl;
+Punto p0 = Punto();
+p0.print();
+cout<<endl;
+
+cout<<"Punto1 por coordenadas"<<endl;
+Punto p1 = Punto(2,3);
+p1.print();
+cout<<endl;
+
+cout<<"Punto2 por copia"<<endl;
+Punto p2 = Punto(p1);
 p2.print();
 cout<<endl;
 
-cout<<"Despues hacemos un swap con los geters y seters p2 vale"<<endl;
+cout<<"Hcemos un swap con los geters y seters p2 vale"<<endl;
 double x,y;
 x = p1.getx();
 y = p1.gety();
@@ -25,18 +34,17 @@ p2.trasladar(2,2);
 p2.print();
 cout<<endl;
 
-cout<<"Igualamos dos puntos: "<<endl;
+cout<<"p2 = p1, p2: "<<endl;
 p2 = p1;
 p2.print();
 cout<<endl;
 
 
-cout<<"Sumandolo al primer punto vale: "<<endl;
+cout<<"p2 = p1+p2, p2: "<<endl;
 p2.trasladar(p1);
 p2.print();
 cout<<endl;
 
-cout<<"P1 no ha variado: "<<endl;
-p1.print();
-
+double distancia = p0.distancia(p1);
+cout<<"La distancia entre p0 y p1 es: "<<distancia<<endl;
 }
--- test_triangulo.cpp
+++ test_triangulo.cpp
@@ -4,20 +4,75 @@
 using namespace std;
 
 int main(){
-double* p = new double[6];
-p[0] = 1;
-p[1] = 2;
-p[2] = 3;
-p[3] = -4;
-p[4] = -4;
-p[5] = 5;
-Triangulo triangulo = Triangulo(p);
-delete[] p;
-triangulo.print();
-double area = triangulo.area();
-cout<<"El area del triangulo es: "<<area<<endl;
-double perimetro = triangulo.perimetro();
-cout<<"El perimetro del triangulo es: "<<perimetro<<endl;
-double calidad = triangulo.calidad();
-cout<<"La calidad del triangulo es: "<<calidad<<endl;
+// Constructores
+cout<<"Triangulo1: por defecto "<<endl;
+Triangulo triangulo1;// Por defecto
+triangulo1.print();
+cout<<endl;
+
+Punto punto1; Punto punto2 = Punto(0,1); Punto punto3 = Punto(1,1);
+cout<<"Triangulo2: dados tres puntos "<<endl;
+Triangulo triangulo2 = Triangulo(punto1,punto2,punto3);// Mediante puntos
+triangulo2.print();
+cout<<endl;
+
+cout<<"Triangulo3: copia del triangulo2 "<<endl;
+Triangulo triangulo3 = triangulo2;// Copia
+triangulo3.print();
+cout<<endl;
+
+Punto *p = &punto1;
+cout<<"Direccion a la que apunta p: "<<p<<endl;
+cout<<"Triangulo4: array puntos "<<endl;
+Triangulo triangulo4 = Triangulo(p);// Array Puntos
+triangulo4.print();
+cout<<endl;
+
+
+double *d = new double[6];
+d[0] =8;d[1]=1;d[2]=2;d[3]=3;d[4]=4;d[5]=5;
+cout<<"Triangulo5: array doubles "<<endl;
+Triangulo triangulo5 = Triangulo(d);// Array doubles
+triangulo5.print();
+cout<<endl;
+
+// Setters
+cout<<"Triangulo1 set= triangulo2 por puntos"<<endl;
+triangulo1.setPtos(punto1,punto2,punto3);
+triangulo1.print();
+cout<<endl;
+
+cout<<"Triangulo4 set= triangulo2 por puntero puntos"<<endl;
+cout<<"Direccion a la que apunta p: "<<p<<endl;
+triangulo4.setPtos(p);
+triangulo4.print();
+cout<<endl;
+
+Punto p4 = Punto(5,5);
+cout<<"Triangulo1 modificada primera coordenada"<<endl;
+triangulo1.setPto(0,p4);
+triangulo1.print();
+cout<<endl;
+
+// Getters
+cout<<"Triangulo1 primera coordenada"<<endl;
+punto1 = triangulo1.getPto(0);
+punto1.print();
+cout<<endl;
+
+// Funciones
+double perimetro;
+double area;
+double calidad;
+cout<<"Estadisticas Triangulo1"<<endl;
+triangulo1.estadisticas(perimetro,area,calidad);
+cout<<"Perimetro: "<<perimetro<<endl;
+cout<<"Area: "<<area<<endl;
+cout<<"Calidad: "<<calidad<<endl;
+cout<<endl;
+// Interseccion
+cout<<"El triangulo2 y el 5 tienen "<<triangulo2.intersecar(triangulo5)<<" vertices en comun"<<endl;
+cout<<"El triangulo1 y el 2 tienen "<<triangulo1.intersecar(triangulo2)<<" vertices en comun"<<endl;
+cout<<"El triangulo2 y el 3 tienen "<<triangulo2.intersecar(triangulo3)<<" vertices en comun"<<endl;
+
 }
